2ba390d0cc62e500a3810677e083612de883bea4
BarRachamin <bar.rachamin15@gmail.com>
2023-03-07 14:10:34 +0200

    fix

--- News.Dal/ConfigDB.cs
+++ News.Dal/ConfigDB.cs
@@ -13,11 +13,9 @@ namespace News.Dal
     {
         public string GetConfigConnectionString()
         {
-           var config = new ConfigurationBuilder().AddJsonFile("appsett dwqrwqeqwings.json", ssdfsdfdsfdsfdsfdsf, optional: true, reloadOnChange: true).Build();
-            
-            
-           string connectionString = config.GetConnectionString("MyDB vvsdfdfedfsdfdsfdsfdsfdsfdsf");
-            
+           var config = new ConfigurationBuilder().AddJsonFile("appsettings.json", optional: true, reloadOnChange: true).Build();
+
+           string connectionString = config.GetConnectionString("MyDB");
 
            return connectionString;
         }
