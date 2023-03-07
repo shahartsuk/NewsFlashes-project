98784bc2413cf8562fb64542f0f7f57acab3a20c
BarRachamin <bar.rachamin15@gmail.com>
2023-03-07 14:15:48 +0200

    try6456

---
+++ News.MyUtilities/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace News.MyUtilities
+{
+    public class Class1
+    {
+    }
+}
